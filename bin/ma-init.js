0090498687f755e45b8a999d469db7533cb3a7a3
varsha <wangshuonpu@163.com>
2018-08-08 17:00:34 +0800

    修改代码结构

--- bin/ma-init.js
+++ bin/ma-init.js
@@ -14,6 +14,7 @@ const rm = require('rimraf').sync;
 
 /**
  * 初始化项目
+ *
  * @param {string} projectName 初始化项目所在的文件夹名
  * @param {Object} program 命令行对象
  */
@@ -22,65 +23,55 @@ module.exports = function (projectName = 'dist', program) {
         program = projectName;
         projectName = null;
     }
+
     // 当前命令目录下，判断是否已存在projectName
     if (utils.isExist(`${process.cwd()}/${projectName}`)) {
         utils.log(`创建失败：${projectName}已存在`, 'ERROR');
         return;
     }
+
     if (!program.template) {
-        utils.log('您未指定模板，将默认使用官方空模板', 'WARNING');
+        utils.log('您未指定模板，将默认使用官方原生模板', 'WARNING');
     }
 
     const templateName = program.template || 'empty';
     const tempFilesPath = path.join(home, '.ma-templates');
 
+    fetchTemplate(templateName, tempFilesPath)
+        .then(() => inquirer.prompt([
+            {
+                name: 'appid',
+                message: '请输入appid'
+            }
+        ]))
+        .then(answers => {
+            const metadata = {
+                projectName,
+                appid: answers.appid
+            };
 
-    init();
+            return generator(
+                metadata,
+                tempFilesPath,
+                `${process.cwd()}/${projectName}`
+            );
+        }).then(() => {
+            utils.log('创建成功', 'SUCCESS');
+        }).catch(err => {
+            utils.log(`创建失败：${err}`, 'ERROR');
+        });
+};
 
-    function init() {
-        fetchTemplate()
-            .then(() => {
-                return inquirer.prompt([
-                    {
-                        name: 'appid',
-                        message: '请输入appid'
-                    }
-                ]);
-            }).then(answers => {
-                const metadata = {
-                    projectName,
-                    appid: answers.appid
-                };
-
-                return generator(
-                    metadata,
-                    tempFilesPath,
-                    `${process.cwd()}/${projectName}`
-                );
-            }).then(() => {
-                utils.log('创建成功', 'SUCCESS');
-            }).catch(err => {
-                utils.log(`创建失败：${err}`, 'ERROR');
-            });
+function fetchTemplate(templateName, tempFilesPath) {
+    if (utils.isExist(tempFilesPath)) {
+        rm(tempFilesPath);
     }
-
-    function fetchTemplate() {
-        if (utils.isExist(tempFilesPath)) {
-            rm(tempFilesPath);
-        }
-        return new Promise((resolve, reject) => {
-            download.downloadOfficialZip(templateName, tempFilesPath, { extract: true })
-                .then(() => {
-                    resolve();
-                })
-                .catch(e => {
-                    if (e.statusCode === 404) {
-                        reject(`无法识别模板名称${templateName}`);
-                    } else if (e) {
-                        reject(`无法下载模板${templateName}`);
-                    }
-                });
+    return download.downloadOfficialZip(templateName, tempFilesPath, {extract: true})
+        .catch(e => {
+            if (e && e.statusCode === 404) {
+                throw new Error(`无法识别模板名称${templateName}`);
+            }
+            throw new Error(`无法下载模板${templateName}`);
         });
 
-    }
-};
+}
